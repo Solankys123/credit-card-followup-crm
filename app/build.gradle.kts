2247b20f012ba0bcbcfdbef193e8045589836e5c
sandeep solanki <92093959+Solankys123@users.noreply.github.com>
2026-09-20 15:52:35 +0530

    add workmanager for reminders

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -27,5 +27,6 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-runtime-compose:2.8.7")
     implementation("androidx.room:room-runtime:2.6.1")
     implementation("androidx.room:room-ktx:2.6.1")
+    implementation("androidx.work:work-runtime-ktx:2.10.0")
     ksp("androidx.room:room-compiler:2.6.1")
 }
